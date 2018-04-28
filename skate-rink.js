d8638ec9207e85a8870bf36e68ebd3e8758a1e46
Unknown <479833352@qq.com>
2018-04-28 18:23:34 +0800

    add slope

--- skate-rink.js
+++ skate-rink.js
@@ -6,6 +6,7 @@ const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 const TOTAL_SIZE = 512;
 
+const GRID_SIZE = TOTAL_SIZE * .0625;
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, .1, 10000);
@@ -22,7 +23,6 @@ function axis(color, vec1, vec2, bool) {
   g.vertices.push(vec2);
   let axis = new THREE.Line(g,m);
   axis.computeLineDistances();
-  // axis.position.z = bool ? 0 : -.05;
   axis.position.z = 1;
   scene.add(axis);
 }
@@ -35,12 +35,12 @@ function init() {
   cvs.appendChild(renderer.domElement);
 
   // x-axis
-  for(let x = -TOTAL_SIZE*.5; x <= TOTAL_SIZE*.5; x += (TOTAL_SIZE * .0625)) {
+  for(let x = -TOTAL_SIZE; x <= TOTAL_SIZE; x += GRID_SIZE) {
     axis('red', new THREE.Vector3(-TOTAL_SIZE,x,0), new THREE.Vector3(TOTAL_SIZE,x,0), x===0);
     // axis('red', new THREE.Vector3(-TOTAL_SIZE,0,0), new THREE.Vector3(TOTAL_SIZE,0,0));
   }
   // y-axis
-  for(let y = -TOTAL_SIZE*.5; y <= TOTAL_SIZE*.5; y += (TOTAL_SIZE * .0625)) {
+  for(let y = -TOTAL_SIZE; y <= TOTAL_SIZE; y += GRID_SIZE) {
     axis('green', new THREE.Vector3(y,-TOTAL_SIZE,0), new THREE.Vector3(y,TOTAL_SIZE,0), y===0);
     // axis('green', new THREE.Vector3(0,-TOTAL_SIZE,0), new THREE.Vector3(0,TOTAL_SIZE,0));
   }
@@ -72,34 +72,114 @@ function set_floor() {
 };
 set_floor();
 
-function create_shape_1() {
-  let g = new THREE.CubeGeometry(5, 15, 1);
-  let c_m = new THREE.MeshBasicMaterial({
-    color: '#f90'
+function count_angle(opt) {
+  if(opt.c) {
+    if(opt.a) {
+      return Math.pow(Math.pow(opt.c, 2) - Math.pow(opt.a, 2), .5);
+    } else {
+      return Math.pow(Math.pow(opt.c, 2) - Math.pow(opt.b, 2), .5);
+    }
+  } else {
+    return Math.pow(Math.pow(opt.a, 2) + Math.pow(opt.b, 2), .5);
+  }
+}
+
+function create_slope() {
+  let cube_geometry = new THREE.CubeGeometry(GRID_SIZE, GRID_SIZE * 3, GRID_SIZE);
+  let cube_material = new THREE.MeshBasicMaterial({
+    color: '#09f'
   });
-  let c = new THREE.Mesh(g, c_m);
-  console.log(TOTAL_SIZE * .3125);
-  c.position.x = -TOTAL_SIZE * .3125;
-  c.position.y = -TOTAL_SIZE * (175 / 512);
-  c.position.z = .5;
-  scene.add(c);
+  let cube = new THREE.Mesh(cube_geometry, cube_material);
+  cube.position.x = -GRID_SIZE * 6.5;
+  cube.position.y = -GRID_SIZE * 5.5;
+  cube.position.z = GRID_SIZE * .5;
+  scene.add(cube);
 
-  // let s = new THREE.PlaneGeometry(20, TOTAL_SIZE);
-  // let flb_material = new THREE.MeshLambertMaterial({
-  //   color: '#fff',
-  //   side: THREE.BackSide
-  // });
-  // let floor_back = new THREE.Mesh(flb_geometry, flb_material);
-  // scene.add(floor_back);
-  // polyhedron = createMesh(new THREE.PolyhedronGeometry(vertices,faces,controls.radius),controls.detail));
+  let v = [
+    new THREE.Vector3(GRID_SIZE * -6, GRID_SIZE * -7, GRID_SIZE),
+    new THREE.Vector3(GRID_SIZE * -6, GRID_SIZE * -7, 0),
+    new THREE.Vector3(GRID_SIZE * -3, GRID_SIZE * -7, 0),
+    new THREE.Vector3(GRID_SIZE * -6, GRID_SIZE * -4, GRID_SIZE),
+    new THREE.Vector3(GRID_SIZE * -6, GRID_SIZE * -4, 0),
+    new THREE.Vector3(GRID_SIZE * -3, GRID_SIZE * -4, 0)
+  ];
+  let f = [
+    new THREE.Face3(0, 1, 2),
+    new THREE.Face3(0, 2, 3),
+    new THREE.Face3(3, 2, 5),
+    new THREE.Face3(3, 5, 4)
+  ];
+  let s = new THREE.Geometry();
+  s.vertices = v;
+  s.faces = f;
+  let s_m = new THREE.MeshLambertMaterial({
+    color: '#09f'
+  });
+  let slope = new THREE.Mesh(s, s_m);
+  scene.add(slope);
 };
-create_shape_1();
+create_slope();
+
+function create_surface(x, y, z) {
+  let d = x * .5;
+  x = -x;
+  y /= 2;
+  console.log(y);
+  z /= Math.pow(x, 2);
+
+  let v = [];
+  let f = [];
+  for(let i = 0, j = x; j <= 0; i++, j+=1) {
+    let h = z * Math.pow(j, 2);
+    v.push(new THREE.Vector3(-d, -y, h));
+    v.push(new THREE.Vector3(j + d, -y, h));
+    v.push(new THREE.Vector3(j + d, y, h));
+    v.push(new THREE.Vector3(-d, y, h));
+    if(i) {
+      let k = 4 * i;
+      f.push(new THREE.Face3(k - 4, k    , k + 1));
+      f.push(new THREE.Face3(k - 3, k - 4, k + 1));
+
+      f.push(new THREE.Face3(k - 3, k + 1, k + 2));
+      f.push(new THREE.Face3(k - 2, k - 3, k + 2));
+
+      f.push(new THREE.Face3(k - 2, k + 2, k + 3));
+      f.push(new THREE.Face3(k - 1, k - 2, k + 3));
+    }
+  }
+  // f.push(new THREE.Face3(v.length - 1, v.length - 3, v.length - 2));
+  // f.push(new THREE.Face3(v.length - 4, v.length - 3, v.length - 1));
+
+  let c_g = new THREE.Geometry();
+  c_g.vertices = v;
+  c_g.faces = f;
+  let c_m = new THREE.MeshLambertMaterial({
+    color: '#09f',
+    // side:THREE.DoubleSide
+  });
+  let curve = new THREE.Mesh(c_g, c_m);
+  return curve;
+}
+
+function create_hathpace() {
+  let cube_geometry = new THREE.CubeGeometry(GRID_SIZE, GRID_SIZE * 3, GRID_SIZE * 3);
+  let cube_material = new THREE.MeshBasicMaterial({
+    color: '#09f'
+  });
+  let cube = new THREE.Mesh(cube_geometry, cube_material);
+  cube.position.set(-GRID_SIZE * 6.5, -GRID_SIZE * 1.5, GRID_SIZE * 1.5);
+  scene.add(cube);
+
+  let curve = create_surface(GRID_SIZE * 2, GRID_SIZE * 3, GRID_SIZE * 3);
+  curve.position.set(-GRID_SIZE * 5, -GRID_SIZE * 1.5, .05);
+  scene.add(curve);
+}
+create_hathpace();
 
 
 
 
-
-camera.position.set(-TOTAL_SIZE, 0, TOTAL_SIZE * 2);
+camera.position.set(-TOTAL_SIZE, -TOTAL_SIZE, TOTAL_SIZE);
 
 function rotateView(radian) {
   camera.position.x = TOTAL_SIZE * Math.cos(radian);
